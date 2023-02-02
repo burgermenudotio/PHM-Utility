244a4de0408315b4fbb5fb49148a341852ed58d5
Laurence <mail@bingono.de>
2023-02-02 21:38:20 +0000

    Implemented ExcludeSpecialCharacters switch parameter

--- functions/New-StrongPassword.ps1
+++ functions/New-StrongPassword.ps1
@@ -5,10 +5,18 @@ function New-StrongPassword {
         [Parameter()]
         [ValidateRange(8,128)]
         [Int]
-        $PasswordLength = 16
+        $PasswordLength = 16,
+        # Exclude special characters switch
+        [Parameter()]
+        [switch]
+        $ExcludeSpecialCharacters
     )
 
-    $availableCharacters = 48..126
+    $availableCharacters = 48..57 + 65..90 + 97..122
+
+    if(-not $ExcludeSpecialCharacters){
+        $availableCharacters += 33..47 + 58..64 + 126
+    }
 
     $password = ""
 
