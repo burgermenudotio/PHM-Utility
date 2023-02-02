564c10c5da2cfd754cc080238a72e51a0bb4463a
Laurence <mail@bingono.de>
2023-02-02 21:26:09 +0000

    Init Module

--- PHM-Utility.psm1
+++ PHM-Utility.psm1
@@ -1,7 +1,7 @@
-# Implement your module commands in this script.
+Get-ChildItem -Path "$PSScriptRoot\functions\" -Filter *.ps1 -Recurse |
+ForEach-Object
+{
+    . $_.FullName
+}
 
-
-# Export only the functions using PowerShell standard verb-noun naming.
-# Be sure to list each exported functions in the FunctionsToExport field of the module manifest file.
-# This improves performance of command discovery in PowerShell.
 Export-ModuleMember -Function *-*
